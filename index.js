e067c334474707c05af8ac80f7593592e57cf781
Yash Pandey <knowledge86400@gmail.com>
2021-11-15 13:42:22 +0530

    Angela Code

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-// function handleClick() {
-//     alert("I got clicked!")
-// }
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.buttonInnerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+
+
+
+/*
+
+// Trash Of Mine
+
+
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
@@ -76,3 +136,44 @@ document.addEventListener(
   },
   false
 );
+
+/*
+  other way of sounding clicks:
+  
+  for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+      var buttonInnerHTML = this.innerHTML;
+
+      switch (buttonInnerHTML) {
+        case "w":
+          var crash = new Audio("./sounds/crash.mp3");
+          crash.play();
+          break;
+        
+        case "a":
+          var kick = new Audio("./sounds/kick-bass.mp3");
+          kick.play();
+          break;
+        
+        // And so on
+        
+        // In the end:
+        default: console.log(this.innerHTML);
+      }
+    })
+  }
+*/
+
+// function BellBoy (name, age, hasWorkPermit, languages) {
+//   this.name = name;
+//   this.age = age;
+//   this.hasWorkPermit = hasWorkPermit;
+//   this.languages = languages;
+//   this.moveSuitcase = function() {
+//     alert("May I take your suitcase?");
+//     pickUpSuitcase();
+//     move();
+//   }
+// }
